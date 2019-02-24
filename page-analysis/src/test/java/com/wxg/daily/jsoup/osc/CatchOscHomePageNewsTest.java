4c97b132905a66b3f8443c6d0524ef4a1e400e71
wxg <chixin0769@163.com>
2019-02-24 19:27:23 +0800

    catch osc news | Test

--- page-analysis/src/test/java/com/wxg/daily/jsoup/osc/CatchOscHomePageNewsTest.java
+++ page-analysis/src/test/java/com/wxg/daily/jsoup/osc/CatchOscHomePageNewsTest.java
@@ -1,5 +1,6 @@
 package com.wxg.daily.jsoup.osc;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -18,6 +19,20 @@ public class CatchOscHomePageNewsTest {
         String url = "http://www.oschina.net";
         CatchOscHomePageNews catchOscHomePageNews =
                 new CatchOscHomePageNews(CatchOscHomePageNews.Type.URL, url);
+
+        System.out.println(catchOscHomePageNews.jsonNews());
+    }
+
+    /**
+     * 从本地的文件中获取资讯（可先使用 page-catch 抓取oschina的首页存储在本地，然后再解析）
+     * `@Ignore` 必须加上这个注解，需要时再打开！
+     */
+    @Test
+    @Ignore
+    public void testFromLocalFile() {
+        String filepath = "D:\\wxg_test\\01\\20190224_161747.603.html";
+        CatchOscHomePageNews catchOscHomePageNews =
+                new CatchOscHomePageNews(CatchOscHomePageNews.Type.LOCAL_FILE, filepath);
 
         System.out.println(catchOscHomePageNews.jsonNews());
     }
