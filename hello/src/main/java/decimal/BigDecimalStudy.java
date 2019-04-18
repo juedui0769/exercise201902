96870647fe829167856bb2510d9b2266a862c3a9
juedui0769 <chixin0769@163.com>
2019-04-18 17:13:41 +0800

    BigDecimalStudy

---
+++ hello/src/main/java/decimal/BigDecimalStudy.java
@@ -0,0 +1,15 @@
+package decimal;
+
+import java.math.BigDecimal;
+
+/**
+ * {@link BigDecimal}
+ * <p>
+ * <a href="https://segmentfault.com/q/1010000009948950">https://segmentfault.com/q/1010000009948950</a>，
+ * 思否上有一个问题，说是使用`BigDecimal`太慢，使用`C`来改写。
+ * </p>
+ */
+public class BigDecimalStudy {
+    public static void main(String[] args) {
+    }
+}
